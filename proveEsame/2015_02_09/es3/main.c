7eb1f02acc8b2a4711728e656b4290d22535128e
Rave <raveggigabriele0@gmail.com>
2022-05-28 22:05:10 +0200

    Ex3 from 2015 exam

--- proveEsame/2015_02_09/es3/main.c
+++ proveEsame/2015_02_09/es3/main.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-#define N 3
-
-
-int ricorsiva(int mat[][n]);
-
-int main(){
-
-	
-
-	return EXIT_SUCCESS;
+#include <stdbool.h>
+ 
+const int N = 4;
+ 
+bool consecutiveRows(int mat[][N], int i, int j) {
+    if(i == N) return false;
+ 
+    if(mat[i][j] == mat[i + 1][j]) {
+        if(j == N - 1) return true;
+        else return consecutiveRows(mat, i, j + 1);
+    }
+    else {
+        return consecutiveRows(mat, i + 1, 0);
+    }
+ 
+    return false;
 }
-
-
-int ricorsiva(int mat[][n])
