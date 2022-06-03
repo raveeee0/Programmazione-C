4a62dcff2b42b11f6d31109d8f5ac6a30d7e1db3
Rave <raveggigabriele0@gmail.com>
2022-06-03 17:43:13 +0200

    Did ex 5

---
+++ proveEsame/2020_01_20/es5/main.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define NUMERO_LETTERE 26
+
+
+typedef struct occorrenza_tag occorrenza;
+struct occorrenza_tag{
+	char lettera;
+	int contatore;
+};
+
+occorrenza *lettera_maggiore_frequenza(FILE *file_ptr);
+
+int main(){
+
+	char *path = "./file.txt";
+	
+	FILE *file_ptr = fopen(path, "r");
+
+	occorrenza *ptr = lettera_maggiore_frequenza(file_ptr);
+	
+	fprintf(stdout, "Lettera: %c, Conteggio: %d\n", ptr->lettera, ptr->contatore);
+	free(ptr);
+
+	return EXIT_SUCCESS;	
+}
+
+occorrenza *lettera_maggiore_frequenza(FILE *file_ptr){
+		
+	if(file_ptr == NULL){
+		perror("Errore nella lettura file");
+		exit(1);
+	}
+	
+	
+	int freq[NUMERO_LETTERE] = {0};
+	char temp;
+	
+	
+	while(fscanf(file_ptr, "%c", &temp) == 1){
+		if(temp >= 97 && temp <= 122)
+			freq[temp - 'a']++;
+		if(temp >= 65 && temp <= 90)
+			freq[temp - 'A']++;	
+	}
+	
+	
+	
+	int max = 0;
+	int index = 0;
+	
+	for(int i = 0; i < 26; i++){
+		if(freq[i] > max){
+			max = freq[i];
+			index = i;
+		}
+	}
+	
+	
+	occorrenza *ptr = (occorrenza *) malloc(sizeof(occorrenza));
+	
+	ptr->lettera = 'a' + index;
+	ptr->contatore = freq[index];
+	
+	
+	return ptr;
+	
+}
+
+
+
+
